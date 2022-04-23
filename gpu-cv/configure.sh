093fe2d7f16a35e3b807724821b70b1733f10d44
khkim <khkim256@gmail.com>
2022-04-23 16:34:36 +0900

    added gpu, gpu-cv

---
+++ gpu-cv/configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed -i -e 's/GPU=0/GPU=1/g' Makefile
+sed -i -e 's/CUDNN=0/CUDNN=1/g' Makefile
+sed -i -e 's/CUDNN_HALF=0/CUDNN_HALF=1/g' Makefile
+sed -i -e 's/OPENCV=0/OPENCV=1/g' Makefile
